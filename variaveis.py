e8192cf088257539f339d69903898c7d58986942
Tomii <brunotb2004@gmail.com>
2026-05-17 01:13:55 -0300

    Feat: Primeiros exemplos de variáveis e tipos de dados

---
+++ variaveis.py
@@ -0,0 +1,10 @@
+nome = "Bruno"
+idade = 21
+altura = 1.90
+estudando_python = True
+
+print(f"Nome: {nome}")
+print(f"idade: {idade} anos")
+print(f"altura: {altura}")
+print(f"Verdade: {estudando_python}")
+
