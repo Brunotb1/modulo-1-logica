3f140c50fa6a18945aad7c90fc289eb4f826ae11
Tomii <brunotb2004@gmail.com>
2026-05-17 21:00:51 -0300

    feat: adiciona loops, índices de lista e desafio da tabuada

---
+++ desafio.py
@@ -0,0 +1,6 @@
+escolha = int(input("Escolha um número para fazermos a tabuada dele: "))
+
+
+for i in range(1, 11):
+    print(f"{escolha} X {i} = {escolha * i}")
+    
---
+++ loops.py
@@ -0,0 +1,18 @@
+for i in range(5):
+    print(i)
+
+
+contador = 0
+
+while contador < 5:
+    print(f"Contando: {contador}")
+    contador += 1
+
+frutas = ["maça", "banana", "uva"]
+
+for fruta in frutas:
+    print(fruta)
+
+print(frutas[1])
+print(frutas[2])
+print(frutas[-1])
